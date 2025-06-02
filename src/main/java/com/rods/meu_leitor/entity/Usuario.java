4e3b97f74fd4e4f1f3b337ccdcf20e07fadfbc96
rods <rods.desenvolvedor@gmail.com>
2025-06-02 09:25:42 -0400

    feat: add base da entidade usuario

---
+++ src/main/java/com/rods/meu_leitor/entity/Usuario.java
@@ -0,0 +1,109 @@
+package com.rods.meu_leitor.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+    public Usuario() {}
+
+    
+    
+    public Usuario(UUID id, String nomeUsuario, String email, String senha, int idade) {
+        this.id = id;
+        this.nomeUsuario = nomeUsuario;
+        this.email = email;
+        this.senha = senha;
+        this.idade = idade;
+    }
+
+        public Usuario(String nomeUsuario, String email, String senha, int idade) {
+        this.nomeUsuario = nomeUsuario;
+        this.email = email;
+        this.senha = senha;
+        this.idade = idade;
+    }
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String nomeUsuario;
+    private String email;
+    private String senha;
+    private int idade;
+
+
+
+
+    public UUID getId() {
+        return id;
+    }
+
+
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public String getSenha() {
+        return senha;
+    }
+
+
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+
+    
+}
