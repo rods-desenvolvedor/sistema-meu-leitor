669b7c08def74da9cf695175225eaced899d6400
rods <rods.desenvolvedor@gmail.com>
2025-06-03 14:40:05 -0400

    feat: add funções para buscar todas as historias de um determinado id

--- src/main/java/com/rods/meu_leitor/repository/HistoriaRepository.java
+++ src/main/java/com/rods/meu_leitor/repository/HistoriaRepository.java
@@ -1,5 +1,7 @@
 package com.rods.meu_leitor.repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.rods.meu_leitor.entity.Historia;
 
 public interface HistoriaRepository extends JpaRepository<Historia, UUID>{
+
+    List<Historia> findAllByUsuarioId(UUID id);
     
 }
