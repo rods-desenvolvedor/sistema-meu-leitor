7542da2cd5019beac9dc75cc17578a6b2d65f62f
rods <rods.desenvolvedor@gmail.com>
2025-06-03 14:36:45 -0400

    feat: add endpoint para cadastar nova historia - mover para outro controller depois

--- src/main/java/com/rods/meu_leitor/controller/HistoriaController.java
+++ src/main/java/com/rods/meu_leitor/controller/HistoriaController.java
@@ -1,10 +1,12 @@
 package com.rods.meu_leitor.controller;
 
+import java.security.Principal;
 import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,14 +26,22 @@ public class HistoriaController {
     }
 
     @GetMapping("/")
-    public String historia(Model model)
+    public String minhasHistorias()
     {
         return "historia/index";
     }
 
-    @PostMapping("/cadastrar")
-    public String cadastrarHistoria()
+    @GetMapping("/nova-historia")
+    public String historia(Model model)
     {
+        model.addAttribute("historiaRequestDto", new HistoriaRequestDto(null, null, null));
+        return "historia/cadastrar";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarHistoria(@ModelAttribute HistoriaRequestDto historiaRequestDto, Principal principal)
+    {
+        historiaService.cadastrarHistoria(principal.getName(), historiaRequestDto);
         return "redirect:/historia/";
     }
     
