5a869896947f6ea80245bbb0dae42efe5f3c2962
rods <rods.desenvolvedor@gmail.com>
2025-06-03 14:39:15 -0400

    feat: add funções para listar historias pelo user logado e pelo id

--- src/main/java/com/rods/meu_leitor/service/HistoriaService.java
+++ src/main/java/com/rods/meu_leitor/service/HistoriaService.java
@@ -3,6 +3,7 @@ package com.rods.meu_leitor.service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import com.rods.meu_leitor.controller.dtos.historiaDto.HistoriaResponseDto;
 import com.rods.meu_leitor.entity.Capitulo;
 import com.rods.meu_leitor.entity.Historia;
 import com.rods.meu_leitor.entity.Usuario;
+import com.rods.meu_leitor.repository.CapituloRepository;
 import com.rods.meu_leitor.repository.HistoriaRepository;
 import com.rods.meu_leitor.repository.UsuarioRepository;
 
@@ -20,18 +22,20 @@ public class HistoriaService {
 
     private final HistoriaRepository historiaRepository;
     private final UsuarioRepository usuarioRepository;
+    private final CapituloRepository capituloRepository;
 
-    public HistoriaService(HistoriaRepository historiaRepository, UsuarioRepository usuarioRepository)
+    public HistoriaService(HistoriaRepository historiaRepository, UsuarioRepository usuarioRepository, CapituloRepository capituloRepository)
     {
         this.historiaRepository = historiaRepository;
         this.usuarioRepository = usuarioRepository;
+        this.capituloRepository = capituloRepository;
     }
 
 
-    public HistoriaResponseDto cadastrarHistoria(UUID idUsuario, HistoriaRequestDto historiaRequestDto)
+    public HistoriaResponseDto cadastrarHistoria(String email, HistoriaRequestDto historiaRequestDto)
     {
 
-        Usuario usuario = usuarioRepository.findById(idUsuario).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+        Usuario usuario = usuarioRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
         
 
         Historia historia  = new Historia(historiaRequestDto.titulo(),
@@ -43,6 +47,24 @@ public class HistoriaService {
         Historia historiaSalva = historiaRepository.save(historia);
 
         return new HistoriaResponseDto(historiaSalva);
+    }
+
+    public List<HistoriaResponseDto> listarHistoriasUsuarioLogado(String email)
+    {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+        .orElseThrow(() -> new RuntimeException("Usuario com esse email não encontrado"));
+
+        List<Historia> historias =  historiaRepository.findAllByUsuarioId(usuario.getId());
+
+        return historias.stream().map(HistoriaResponseDto::new).toList();
+    }
+
+    public HistoriaResponseDto listarHistoriaPeloId(UUID id)
+    {
+        Historia historia = historiaRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Historia não encontrada"));
+
+        return new HistoriaResponseDto(historia);
     }
     
 }
