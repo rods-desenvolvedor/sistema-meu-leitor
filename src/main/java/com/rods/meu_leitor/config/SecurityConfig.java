811e3979f360f96145508193dc6aa2a868f98a54
rods <rods.desenvolvedor@gmail.com>
2025-06-03 09:15:49 -0400

    feat: add função basica de configuração do spring security

---
+++ src/main/java/com/rods/meu_leitor/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.rods.meu_leitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/usuario/cadastrar", "/usuario/", "/login", "/css/**", "/home")
+        .permitAll().anyRequest().authenticated()
+        ).formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/home", true).permitAll()
+        ).logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEnconder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
