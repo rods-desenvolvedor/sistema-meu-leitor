07d53644575e31461783566c5f030c125cbb656b
rods <rods.desenvolvedor@gmail.com>
2025-06-03 09:15:04 -0400

    feat: add função para encontrar usuario pelo email

--- src/main/java/com/rods/meu_leitor/repository/UsuarioRepository.java
+++ src/main/java/com/rods/meu_leitor/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.rods.meu_leitor.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.rods.meu_leitor.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID>{
+
+    Optional<Usuario> findByEmail(String email);
     
 }
