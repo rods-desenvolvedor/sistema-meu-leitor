08340b4c663b0707f2ed4a854db48e7b99ac70fd
rods <rods.desenvolvedor@gmail.com>
2025-06-03 14:41:24 -0400

    feat: dto historia agora retorna os capitulos

--- src/main/java/com/rods/meu_leitor/controller/dtos/historiaDto/HistoriaResponseDto.java
+++ src/main/java/com/rods/meu_leitor/controller/dtos/historiaDto/HistoriaResponseDto.java
@@ -1,14 +1,18 @@
 package com.rods.meu_leitor.controller.dtos.historiaDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
+import com.rods.meu_leitor.controller.dtos.capituloDto.CapituloResponseDto;
 import com.rods.meu_leitor.entity.Historia;
 
-public record HistoriaResponseDto(String titulo, String descricao, LocalDateTime dataCriacao) {
+public record HistoriaResponseDto(UUID id,String titulo, String descricao, LocalDateTime dataCriacao, List<CapituloResponseDto> capitulos) {
 
     public HistoriaResponseDto(Historia historia)
     {
-        this(historia.getTitulo(), historia.getDescricao(), historia.getDataCriacao());
+        this(historia.getId(),historia.getTitulo(), historia.getDescricao(), historia.getDataCriacao(),
+        historia.getCapitulos().stream().map(CapituloResponseDto::new).toList());
     }
     
 }
