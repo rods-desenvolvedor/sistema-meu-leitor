09eccd2dd0374c815a899ad791e4edb0ef818d73
rods <rods.desenvolvedor@gmail.com>
2025-06-04 11:45:12 -0400

    feat: limpeza de codigo

--- src/main/java/com/rods/meu_leitor/service/HistoriaService.java
+++ src/main/java/com/rods/meu_leitor/service/HistoriaService.java
@@ -3,7 +3,6 @@ package com.rods.meu_leitor.service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
