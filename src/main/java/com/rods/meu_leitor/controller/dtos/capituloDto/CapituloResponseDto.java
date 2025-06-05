e54e9fbafab74388a865898f8b88aa72180a389d
rods <rods.desenvolvedor@gmail.com>
2025-06-04 21:54:12 -0400

    feat: response dto do capitulo agora retorna o id

--- src/main/java/com/rods/meu_leitor/controller/dtos/capituloDto/CapituloResponseDto.java
+++ src/main/java/com/rods/meu_leitor/controller/dtos/capituloDto/CapituloResponseDto.java
@@ -1,12 +1,14 @@
 package com.rods.meu_leitor.controller.dtos.capituloDto;
 
+import java.util.UUID;
+
 import com.rods.meu_leitor.entity.Capitulo;
 
-public record CapituloResponseDto(String titulo, String conteudo) {
+public record CapituloResponseDto(UUID id,String titulo, String conteudo) {
 
     public CapituloResponseDto(Capitulo capitulo)
     {
-        this(capitulo.getTitulo(), capitulo.getConteudo());
+        this(capitulo.getId(),capitulo.getTitulo(), capitulo.getConteudo());
     }
     
 }
