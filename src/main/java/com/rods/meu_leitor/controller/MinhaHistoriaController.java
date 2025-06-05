d13693b1210fbbbbcd58c79ae7cd42ab3c606aec
rods <rods.desenvolvedor@gmail.com>
2025-06-04 21:53:12 -0400

    feat: add endpoints para cadastrar e editar capitulos

--- src/main/java/com/rods/meu_leitor/controller/MinhaHistoriaController.java
+++ src/main/java/com/rods/meu_leitor/controller/MinhaHistoriaController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.rods.meu_leitor.controller.dtos.capituloDto.CapituloRequestDto;
 import com.rods.meu_leitor.controller.dtos.historiaDto.HistoriaRequestDto;
 import com.rods.meu_leitor.service.HistoriaService;
 
@@ -43,9 +46,17 @@ public class MinhaHistoriaController {
     }
 
     @GetMapping("/editar/{id}")
-    public String editarCapitulo(@PathVariable UUID id)
+    public String formularioEditarCapitulo(@PathVariable UUID id, Model model)
     {
+        model.addAttribute("capitulo", historiaService.buscarCapituloPeloId(id));
         return "minha-historia/editar-capitulo";
+    }
+
+    @PostMapping("/editar/{id}")
+    public String salvarCapituloEditado(@PathVariable UUID id, @ModelAttribute CapituloRequestDto capituloRequestDto)
+    {
+        historiaService.editarCapitulo(id, capituloRequestDto);
+        return "redirect:/minha-historia/listar";
     }
 
     @GetMapping("/form-cadastrar")
@@ -60,6 +71,22 @@ public class MinhaHistoriaController {
     {
         historiaService.cadastrarHistoria(principal.getName(), historiaRequestDto);
         return "redirect:/minha-historia/listar";
+    }
+
+    @GetMapping("/form-cadastrar-capitulo/{id}")
+    public String formularioCadastrarCapitulo(Model model, @PathVariable UUID id)
+    {
+        model.addAttribute("capituloRequestDto", new CapituloRequestDto(null, null));
+        model.addAttribute("idHistoria", id);
+        return "minha-historia/novo-capitulo";
+    }
+
+    @PostMapping("/cadastrar-capitulo")
+    public String cadastrarCapitulo(@RequestParam UUID id ,Model model, @ModelAttribute CapituloRequestDto capituloRequestDto)
+    {
+        historiaService.cadastrarCapitulo(capituloRequestDto, id);
+        model.addAttribute("historia", historiaService.listarHistoriaPeloId(id));
+        return "minha-historia/continuar-escrevendo";
     }
 
     
